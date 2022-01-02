31b7e520f6a0104fcb1a36855fdbc7caa2a60ba3
marcoskloss <marcoskloss2001@gmail.com>
2022-01-02 16:54:39 -0300

    feat: add Manrope font

--- src/components/Theme/GlobalStyle.js
+++ src/components/Theme/GlobalStyle.js
@@ -2,11 +2,16 @@ import { createGlobalStyle } from 'styled-components'
 import { background, font } from './helpers'
 
 export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap');
+
     * {
         box-sizing: border-box;
         margin: 0;
         padding: 0;
         -webkit-font-smoothing: antialiased;
+        font-family: Manrope, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+        'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+        'Helvetica Neue', sans-serif;
     }
 
     html, body, #root {
@@ -16,9 +21,6 @@ export const GlobalStyle = createGlobalStyle`
     body {
         ${background};
         ${font};
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
-        'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-        'Helvetica Neue', sans-serif;
     }
 
     #root {
