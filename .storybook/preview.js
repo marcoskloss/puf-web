cca0178c5ccbaecd09133b8ba7355f4b50ef7e18
Marcos Kloss <marcoskloss2001@gmail.com>
2022-03-21 14:14:19 -0300

    docs: configure Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+import * as React from 'react'
+import { Theme } from '~/components'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
 }
+
+export const decorators = [
+    Story => (
+        <Theme>
+            <div>
+                <Story />
+            </div>
+        </Theme>
+    ),
+]
