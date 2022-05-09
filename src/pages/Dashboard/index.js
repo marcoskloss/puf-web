572e28b04b383dc0000827ce78ed1fd3c5baea7b
Marcos Kloss <marcoskloss2001@gmail.com>
2022-05-08 22:01:29 -0300

    feat: add dashboard page basic structure

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,31 @@
 import * as React from 'react'
-import { useAuth } from '~/modules'
+import styled from 'styled-components'
+
+import { th, Logo } from '~/components'
+
+const Container = styled.div`
+    display: flex;
+    flex: 1;
+`
+
+const Main = styled.main`
+    flex: 1;
+    max-width: 1280px;
+    margin: 0 auto;
+`
+
+const Menu = styled.aside`
+    background-color: ${th.color('black')};
+    padding: ${th.space(2)}px;
+`
 
 export const Dashboard = () => {
-    const [auth, { logout }] = useAuth()
-
     return (
-        <div>
-            <button onClick={logout}>sair</button>
-            <div>olá {auth.user.name}</div>
-        </div>
+        <Container>
+            <Menu>
+                <Logo onlyIcon height={50} />
+            </Menu>
+            <Main>content</Main>
+        </Container>
     )
 }
